2d434c0e43fed4f784fb9c3d63503fc42d095dbf
Şafak Şahin <safaksahin022@gmail.com>
2024-02-26 14:46:06 +0300

    Proje dosyası ekle.

---
+++ KidKinder/Controllers/DefaultController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KidKinder.Entities;
+using KidKinder.Context;
+
+namespace KidKinder.Controllers
+{
+    public class DefaultController : Controller
+    {
+        KidKinderContext context = new KidKinderContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            var values = context.Features.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialService()
+        {
+            var values = context.Services.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialClassRooms()
+        {
+            var values = context.ClassRooms.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialBookASeat()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTeacher()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTestimonial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ KidKinder/Entities/ClassRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KidKinder.Entities
+{
+    public class ClassRoom
+    {
+        public int ClassRoomId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AgeOfKids { get; set; }
+        public byte TotalSeats { get; set; }
+        public string ClassTime { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

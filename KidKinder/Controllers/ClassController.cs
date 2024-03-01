54e1effaca02fdaef55d47805f30ceafc910e44e
Şafak Şahin <safaksahin022@gmail.com>
2024-03-01 20:22:25 +0300

    teacher

---
+++ KidKinder/Controllers/ClassController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidKinder.Controllers
+{
+    public class ClassController : Controller
+    {
+        // GET: Classes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult ClassHeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}

da6b3006ff685e78386a9d391dd97acc9706c508
Şafak Şahin <safaksahin022@gmail.com>
2024-03-01 18:58:37 +0300

    about

---
+++ KidKinder/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidKinder.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AboutHeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}

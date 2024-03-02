eaf02b4b46c49208c8f2d1c2a3bf2c66c2dd9ca8
Şafak Şahin <safaksahin022@gmail.com>
2024-03-02 21:38:58 +0300

    aboutList

---
+++ KidKinder/Controllers/AdminAboutController.cs
@@ -0,0 +1,40 @@
+﻿using KidKinder.Context;
+using KidKinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidKinder.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        KidKinderContext context = new KidKinderContext();
+        public ActionResult Index()
+        {
+            var values = context.Abouts.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult UpdateAbout(int id)
+        {
+            var values = context.Abouts.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAbout(About about)
+        {
+            var values = context.Abouts.Find(about.AboutId);
+            values.Title = about.Title;
+            values.Header = about.Header;
+            values.Description = about.Description;
+            values.BigImageUrl = about.BigImageUrl;
+            values.SmallImageUrl = about.SmallImageUrl;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ KidKinder/Controllers/AdminAboutListController.cs
@@ -0,0 +1,36 @@
+﻿using KidKinder.Context;
+using KidKinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidKinder.Controllers
+{
+    public class AdminAboutListController : Controller
+    {
+        KidKinderContext context = new KidKinderContext();
+        public ActionResult Index()
+        {
+            var values = context.AboutLists.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult UpdateAboutList(int id)
+        {
+            var values = context.AboutLists.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAboutList(AboutList aboutList)
+        {
+            var values = context.AboutLists.Find(aboutList.AboutListId);
+            values.Description = aboutList.Description;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
